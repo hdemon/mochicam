f498b565c759842c779692b9580f0fd291347f4f
Masami Yonehara <prosphylverb@gmail.com>
2021-07-13 15:59:37 +0900

    update

--- roles/rtmp-streaming/files/launch-rtmp-streamer.sh
+++ roles/rtmp-streaming/files/launch-rtmp-streamer.sh
@@ -4,7 +4,7 @@ sudo ffmpeg -f alsa -ac 1 -thread_queue_size 8192 -i hw:1,0 \
             -f v4l2 -thread_queue_size 8192 -input_format yuyv422 \
             -i /dev/video0 -c:v h264_omx \
             -framerate 60 \
-            -video_size 1024x768 \
+            -video_size 800x600 \
             -c:v h264_omx \
             -b:v 768k -bufsize 768k \
             -vsync 1 -g 16 \
